35fe71764ead9db30d45accf1d20aa96bb42fe6b
dcahun <dcahun@frd.org.mx>
2026-02-18 16:08:23 -0600

    se agregan fonts y funcion para calcular fecha

--- app/src/main/java/com/example/habito66/feature/home/components/HeaderHomeSection.kt
+++ app/src/main/java/com/example/habito66/feature/home/components/HeaderHomeSection.kt
@@ -8,13 +8,17 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.example.habito66.ui.theme.InterBoldText
+import com.example.habito66.ui.theme.InterMediumText
+import com.example.habito66.utils.formattedDate
 
 @Composable
 fun HeaderHomeSection() {
@@ -26,8 +30,9 @@ fun HeaderHomeSection() {
         verticalAlignment = Alignment.CenterVertically
     ) {
         Column {
-            Text(text = "El viaje de hoy", style = MaterialTheme.typography.headlineSmall)
-            Text(text = "16 de Febrero, 2026", style = MaterialTheme.typography.bodyMedium) // Fecha dinámica iría aquí
+            Text(text = "El viaje de hoy", style = InterBoldText)
+            val dateText = remember { formattedDate() }
+            Text(text = dateText, style = InterMediumText, color = colorScheme.primary)
         }
         // Placeholder del icono de perfil
         Box(modifier = Modifier.size(48.dp).background(Color.Gray)) {
@@ -35,3 +40,5 @@ fun HeaderHomeSection() {
         }
     }
 }
+
+
